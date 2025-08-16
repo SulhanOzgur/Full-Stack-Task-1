f1e2baf4d25cd20bfba98d48c2bc38f7edfcec94
SulhanOzgur <sulhan_ozgur@hotmail.com>
2025-08-16 23:55:41 +0300

    Enable CORS for Frontend (localhost:3000)

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,84 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using ProductApi.Data;
+using ProductApi.Repositories;
+using ProductApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// PostgreSQL + EF Core
+// -------------------- Services --------------------
+
+// DbContext (PostgreSQL)
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+// Katmanlı mimari DI kayıtları
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 // Controllers + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// (Next.js bağlayınca CORS aç!!!)
-// builder.Services.AddCors(opt => opt.AddPolicy("frontend",
-//     p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod()));
+// Frontend için CORS (Next.js: http://localhost:3000)
+builder.Services.AddCors(opt =>
+    opt.AddPolicy("frontend",
+        p => p.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()));
+
+// ---------------------------------------------------
 
 var app = builder.Build();
 
+// Swagger sadece Development’ta
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// app.UseCors("frontend");
 app.UseHttpsRedirection();
+
+// CORS politikasını etkinleştirdim
+app.UseCors("frontend");
+
 app.MapControllers();
-app.Run();
-
-
-
-
-
-
-
-/* using Microsoft.EntityFrameworkCore;
-using ProductApi.Data;
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-*/
