b696a4df263236ec63e00ebe68f6ca01b526d31b
SulhanOzgur <sulhan_ozgur@hotmail.com>
2025-08-21 16:33:07 +0300

    Update AppDbContext and add EF tooling deps for migrations

--- server/src/Infrastructure/Data/AppDbContext.cs
+++ server/src/Infrastructure/Data/AppDbContext.cs
@@ -1,18 +1,32 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Data;
-
-public class AppDbContext : DbContext
+namespace Infrastructure.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<User> Users => Set<User>();
-    public DbSet<Product> Products => Set<Product>();
-
-    protected override void OnModelCreating(ModelBuilder b)
+    public class AppDbContext : DbContext
     {
-        b.Entity<User>().HasIndex(x => x.Email).IsUnique();
-        b.Entity<Product>().HasIndex(x => new { x.Name, x.Category });
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(b =>
+            {
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Email).IsRequired();
+                b.HasIndex(x => x.Email).IsUnique();
+            });
+
+            modelBuilder.Entity<Product>(b =>
+            {
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Name).IsRequired();
+                b.HasIndex(x => new { x.Name, x.Category });
+            });
+        }
     }
 }
